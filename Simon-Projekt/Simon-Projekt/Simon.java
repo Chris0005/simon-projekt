8e10ea7cb4316e98683055d8f924e572d2aac60d
Chris0005 <113801918+Chris0005@users.noreply.github.com>
2022-10-17 09:08:40 +0200

    Add files via upload

---
+++ Simon-Projekt/Simon-Projekt/Simon.java
@@ -0,0 +1,24 @@
+public class Simon
+{
+    Levels l = new Levels();
+    public boolean spielEnde;
+    
+    public boolean spielen(){
+        int i = 1;
+        while(spielEnde != true){
+            erstelleLevel(i);
+        }
+        
+        return true;
+    }
+    
+    
+    
+    public void erstelleLevel(int levelAnzahl){
+        
+        
+        
+    }
+    
+    
+}
